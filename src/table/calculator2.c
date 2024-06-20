2f5cbe835a5b0a09f5379daa599402aca6363da0
sfaiz801 <sfaiz777@rediffemail.com>
2024-06-20 09:51:32 +0530

    chenges X

---
+++ src/table/calculator2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() 
+{
+    char operator;  
+    int num1, num2; 
+
+    while (1)
+    {  
+        printf("Enter an operator (+, -, *, /), or press x to exit: ");
+        scanf(" %c", &operator);  
+
+        if (operator == 'x') 
+        { 
+            break;
+        }
+
+        printf("Enter two numbers: ");
+        scanf("%d %d", &num1, &num2);  
+
+        if (operator == '+')
+        {
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);  
+        } else if (operator == '-') 
+        {
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);  
+        } else if (operator == '*') 
+        {
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);  
+        } else if (operator == '/') 
+        {
+            printf("%d / %d = %d\n", num1, num2, num1 / num2);  
+        } else {
+            printf("Invalid operator.\n");  
+        }
+    }
+
+    return 0;  
+}
